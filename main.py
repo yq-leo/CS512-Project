143cb022a8af49ac92d6356da5a638b7cd5f71fa
qiyu6 <qiyu6@illinois.edu>
2024-04-12 19:05:13 -0500

    update code

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from torch.optim.lr_scheduler import CosineAnnealingLR
-from torch.utils.tensorboard import SummaryWriter
-
 from utils import *
 from args import *
 from model import *
@@ -31,59 +28,14 @@ if __name__ == '__main__':
     G1_data = build_tg_graph(G1.number_of_nodes(), edge_index1, x1, anchor_links[:, 0], dists_score1).to(device)
     G2_data = build_tg_graph(G2.number_of_nodes(), edge_index2, x2, anchor_links[:, 1], dists_score2).to(device)
 
-    # model setting
-    model_settings = {
-        "input_dim": G1_data.x.shape[1],
-        "feature_dim": args.feat_dim,
-        "anchor_dim": args.c * int(np.log2(num_anchor_links)) ** 2 if args.random else num_anchor_links,
-        "hidden_dim": args.hidden_dim,
-        "output_dim": args.out_dim,
-        "feature_pre": args.feature_pre,
-        "num_layers": args.num_layers,
-        "use_dropout": args.use_dropout,
-        "dist_trainable": args.dist_trainable,
-        "use_hidden": args.use_hidden,
-        "mcf_type": args.mcf_type,
-        "agg_type": args.agg_type
-    }
-    print(f"Model settings: {model_settings}")
+    # compute OT cost
+    print("Computing OT cost...")
+    cost_rwr = compute_ot_cost_matrix(G1_data, G2_data).cpu().numpy()
+    hits, mrr = compute_metrics(cost_rwr[test_pairs[:, 0]], cost_rwr.T[test_pairs[:, 1]], test_pairs)
+    # distances1 = compute_distance_matrix(dists_score1[test_pairs[:, 0]], dists_score2, dist_type=args.dist_type)
+    # distances2 = compute_distance_matrix(dists_score2[test_pairs[:, 1]], dists_score1, dist_type=args.dist_type)
+    # hits, mrr = compute_metrics(distances1, distances2, test_pairs)
+    print(f'{", ".join([f"Hits@{key}: {value:.4f}" for (key, value) in hits.items()])}, MRR: {mrr:.4f}')
 
-    model = PGNN(**model_settings).to(device)
-    # model = BRIGHT_U(anchor_dim, out_dim).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-    criterion = RankingLossL1(args.neg_sample_size, args.margin, args.dist_type).to(device)
-    scheduler = CosineAnnealingLR(optimizer, T_max=args.epochs)
 
-    # train model
-    if not os.path.exists('logs'):
-        os.makedirs('logs')
-    writer = SummaryWriter(log_path(args.dataset))
 
-    for epoch in range(args.epochs):
-        # training
-        model.train()
-        optimizer.zero_grad()
-        G1_data.dists_max, G1_data.dists_argmax, G2_data.dists_max, G2_data.dists_argmax = (
-            preselect_anchor(G1_data, G2_data, random=args.random, c=args.c, device=device))
-        out1, out2 = model(G1_data, G2_data)
-        loss = criterion(out1, out2, G1_data.anchor_nodes, G2_data.anchor_nodes)
-        loss.backward()
-        optimizer.step()
-        print(f'Epoch: {epoch + 1}, Loss: {loss.item():.6f}', end=" ")
-
-        # testing
-        out1_np = out1.detach().cpu().numpy()
-        out2_np = out2.detach().cpu().numpy()
-        distances1 = compute_distance_matrix(out1_np[test_pairs[:, 0]], out2_np, dist_type=args.dist_type)
-        distances2 = compute_distance_matrix(out2_np[test_pairs[:, 1]], out1_np, dist_type=args.dist_type)
-        hits, mrr = compute_metrics(distances1, distances2, test_pairs)
-        print(f'{", ".join([f"Hits@{key}: {value:.4f}" for (key, value) in hits.items()])}, MRR: {mrr:.4f}')
-
-        writer.add_scalar('Loss', loss.item(), epoch)
-        writer.add_scalar('MRR', mrr, epoch)
-        for key, value in hits.items():
-            writer.add_scalar(f'Hits@{key}', value, epoch)
-
-        scheduler.step()
-
-    writer.close()
--- utils/metrics.py
+++ utils/metrics.py
@@ -2,6 +2,9 @@ import numpy as np
 import scipy
 import os
 
+import torch
+from torch_geometric.utils import to_dense_adj, degree
+
 
 def compute_distance_matrix(embedding1, embedding2, dist_type='l1'):
     """
@@ -17,6 +20,38 @@ def compute_distance_matrix(embedding1, embedding2, dist_type='l1'):
         return scipy.spatial.distance.cdist(embedding1, embedding2, metric='cityblock')
     elif dist_type == 'cosine':
         return scipy.spatial.distance.cdist(embedding1, embedding2, metric='cosine')
+
+
+def compute_ot_cost_matrix(G1_data, G2_data):
+    """
+    Compute optimal transport cost matrix between two sets of embeddings
+    :param G1_data: PyG Data object for graph 1
+    :param G2_data: PyG Data object for graph 2
+    :return: cost_rwr: cost matrix
+    """
+
+    alpha = 1
+    beta = 0.5
+    gamma = 0.8
+
+    r1, r2 = G1_data.dists, G2_data.dists
+    x1, x2 = G1_data.x, G2_data.x
+    cost_node = alpha * np.exp(-r1 @ r2.T) + (1-alpha) * np.exp(-x1 @ x2.T)
+
+    A1, A2 = to_dense_adj(G1_data.edge_index)[0], to_dense_adj(G2_data.edge_index)[0]
+    D1, D2 = torch.diag(degree(G1_data.edge_index[0])), torch.diag(degree(G2_data.edge_index[0]))
+    W1, W2 = (torch.inverse(D1) @ A1).T, (torch.inverse(D2) @ A2).T
+
+    cost_rwr = np.copy(cost_node)
+    cnt = 0
+    while True:
+        cost_rwr_prev = np.copy(cost_rwr)
+        cost_rwr = (1+beta) * cost_node + (1-beta) * gamma * (W1 @ cost_rwr @ W2.T)
+        if np.linalg.norm(cost_rwr - cost_rwr_prev) < 1e-6:
+            break
+        cnt += 1
+    print(f"OT Cost converged in {cnt} iterations")
+    return cost_rwr
 
 
 def compute_metrics(distances1, distances2, test_pairs, hit_top_ks=(1, 5, 10, 30, 50, 100)):
